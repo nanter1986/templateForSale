c7eca6a0c2145c4bc8d28617a1e8247f76a835c9
nanter1986 <nanter1986@gmail.com>
2018-01-23 10:20:33 +0200

    image animation

--- js/index.js
+++ js/index.js
@@ -7,3 +7,19 @@ $(document).ready(function () {
     });
 
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
