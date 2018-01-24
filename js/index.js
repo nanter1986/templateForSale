689a8da2ecc6191389712238627253f353cfe447
nanter1986 <nanter1986@gmail.com>
2018-01-24 09:22:48 +0200

    optimized jquery

--- js/index.js
+++ js/index.js
@@ -6,22 +6,15 @@ $(document).ready(function () {
         $(this).addClass("active");
     });
 
-});
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if (height > 100) {
+            $('#back2Top').fadeIn();
+        } else {
+            $('#back2Top').fadeOut();
+        }
+    });
 
-window.onscroll = function() {scrollFunction()};
-
-
-// When the user clicks on the button, scroll to the top of the document
-/*Scroll to top when arrow up clicked BEGIN*/
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('#back2Top').fadeIn();
-    } else {
-        $('#back2Top').fadeOut();
-    }
-});
-$(document).ready(function() {
     $("#back2Top").click(function(event) {
         event.preventDefault();
         $("html, body").animate({ scrollTop: 0 }, "slow");
@@ -29,4 +22,15 @@ $(document).ready(function() {
     });
 
 });
+
+//window.onscroll = function() {scrollFunction()};
+
+
+// When the user clicks on the button, scroll to the top of the document
+/*Scroll to top when arrow up clicked BEGIN*/
+
+// $(document).ready(function() {
+//
+//
+// });
  /*Scroll to top when arrow up clicked END*/
