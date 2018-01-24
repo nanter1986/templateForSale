b69e73fcd58c296d9288e969d0f5b3bc961408d3
nanter1986 <nanter1986@gmail.com>
2018-01-24 09:48:51 +0200

    search html

--- js/index.js
+++ js/index.js
@@ -22,15 +22,3 @@ $(document).ready(function () {
     });
 
 });
-
-//window.onscroll = function() {scrollFunction()};
-
-
-// When the user clicks on the button, scroll to the top of the document
-/*Scroll to top when arrow up clicked BEGIN*/
-
-// $(document).ready(function() {
-//
-//
-// });
- /*Scroll to top when arrow up clicked END*/
