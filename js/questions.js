12c9a6b863b77b9ca23dca2e0d8af5d0e5a7da70
nanter1986 <nanter1986@gmail.com>
2018-01-25 12:45:15 +0200

    link to eshop

---
+++ js/questions.js
@@ -0,0 +1,18 @@
+const questions=[
+  {
+    question:"Τι ποσοστό των καταναλωτών κάνει έρευνα στο διαδίκτυο πριν πραγματοποιήσει αγορές?",
+    first:"21%",
+    two:"41%",
+    three:"61%",
+    four:"81%",
+    correct:"81%"
+  },
+  {
+    question:"What is my name?",
+    first:"john",
+    two:"nanter",
+    three:"mike",
+    four:"terry",
+    correct:"nanter"
+  }
+]
