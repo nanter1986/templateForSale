564bf6824c5d4c28a60a2866be078e17cfd6a0ab
nanter1986 <nanter1986@gmail.com>
2018-01-25 12:32:58 +0200

    questions work

---
+++ js/questions.js
@@ -0,0 +1,18 @@
+const questions=[
+  {
+    question:"What is the first number?",
+    first:"one",
+    two:"two",
+    three:"three",
+    four:"four",
+    correct:"one"
+  },
+  {
+    question:"What is my name?",
+    first:"john",
+    two:"nanter",
+    three:"mike",
+    four:"terry",
+    correct:"nanter"
+  }
+]
